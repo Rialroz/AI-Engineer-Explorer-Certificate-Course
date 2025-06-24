eab6dc1c8be549817e86e44e96a00e5e577fcdcd
Rialroz <96717337+Rialroz@users.noreply.github.com>
2025-06-24 09:14:14 +0700

    Update add_practice1.py

--- Python_Programming_Basics_for_Artificial_Intelligence/day5/add_practice1.py
+++ Python_Programming_Basics_for_Artificial_Intelligence/day5/add_practice1.py
@@ -1,10 +1,10 @@
-def count_vowels(s):
+def count_vowels(teks):
     vowels = 'aeiouAEIOU'
     count = 0
-    for char in s:
+    for char in teks:
         if char in vowels:
             count += 1
     return count
 
-s = input("Enter a string: ")
-print(f"The number of vowels in the string is: {count_vowels(s)}")
+teks = input("Enter a string: ")
+print(f"The number of vowels in the string is: {count_vowels(teks)}")
