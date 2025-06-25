aac1b2f138cb58bb030070043098b492b6996f77
Rialroz <96717337+Rialroz@users.noreply.github.com>
2025-06-25 09:28:47 +0700

    Update add_practice1.py

--- Python_Programming_Basics_for_Artificial_Intelligence/day6/add_practice1.py
+++ Python_Programming_Basics_for_Artificial_Intelligence/day6/add_practice1.py
@@ -1,6 +1,6 @@
 def read_items_file(filename):
     try:
-        with open(filename, "r") as file:
+        with open(filename, as file:
             items = [item.strip() for item in file.readlines()]
             print("Items in the file:")
             for item in items:
@@ -15,8 +15,8 @@ def write_items_file(filename, items):
             file.write(item + "\n")
 
 # Example usage
-buah = read_items_file(r"Python_Programming_Basics_for_Artificial_Intelligence/Day6/fruits.txt")
+buah = read_items_file("fruits.txt")
 print("Items read from file:", buah)
-write_items_file(r"Python_Programming_Basics_for_Artificial_Intelligence/Day6/practice1.txt", buah)
-read_items_file(r"Python_Programming_Basics_for_Artificial_Intelligence/Day6/practice1.txt")
+write_items_file("practice1.txt", buah)
+read_items_file("practice1.txt")
 # This code reads items from a file, writes them to another file, and then reads from that new file.
